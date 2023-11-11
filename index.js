2b159abd40c217cc5cc2eabc7ab3194bc9de9330
Colin C <ciciccz@gmail.com>
2023-11-11 15:36:34 -0700

    added allItems endpoint

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { graphqlHandler, graphqlError } = require('graphql-serverless')
-const { makeExecutableSchema } = require('graphql-tools') 
+const { makeExecutableSchema } = require('graphql-tools')
 const { app } = require('webfunc')
 
 const itemMocks = [
-  { id: 1, name: 'Knock Bits', quantity: 88, price: 12.67, supplier_id: 1 }, 
+  { id: 1, name: 'Knock Bits', quantity: 88, price: 12.67, supplier_id: 1 },
   { id: 2, name: 'Widgets', quantity: 10, price: 35.50, supplier_id: 3 }]
 
 const schema = `
@@ -17,19 +17,27 @@ const schema = `
  
   type Query {
       itemsById(id: Int): Item
+      allItems: [Item]
   }
   `
- 
+
 const itemResolver = {
   Query: {
-      itemsById(root, { id }, context) {
-		const results = id ? itemMocks.filter(p => p.id == id) : itemMocks
-		      if (results.length > 0)
-			return results.pop()
-		      else
-			throw graphqlError(404, `Item with id ${id} does not exist.`)
-      }
-   }
+    itemsById(root, { id }, context) {
+      const results = id ? itemMocks.filter(p => p.id == id) : itemMocks
+      if (results.length > 0)
+        return results.pop()
+      else
+        throw graphqlError(404, `Item with id ${id} does not exist.`)
+    },
+    allItems(root, args, context) {
+      const results = itemMocks;
+      if (results.length > 0)
+        return results
+      else
+        throw graphqlError(404, `Item does not exist.`)
+    }
+  }
 }
 
 const executableSchema = makeExecutableSchema({
@@ -39,14 +47,14 @@ const executableSchema = makeExecutableSchema({
 
 const graphqlOptions = {
   schema: executableSchema,
-  graphiql: { 
-    endpoint: '/graphiql' 
+  graphiql: {
+    endpoint: '/graphiql'
   },
   context: {
-  	someVar: 'This variable is passed in the "context" object in each resolver.'
+    someVar: 'This variable is passed in the "context" object in each resolver.'
   }
 }
 
-app.all(['/','/graphiql'], graphqlHandler(graphqlOptions))
+app.all(['/', '/graphiql'], graphqlHandler(graphqlOptions))
 
 eval(app.listen('app', 4000))
